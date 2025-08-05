8b8a7dcee1dda1acf702ff6fe509b3a687409a4e
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-08-05 16:55:57 -0700

    Initial Commit

---
+++ SurfaceIntegrityChecker/SurfaceIntegrityCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfaceIntegrityCheckerApp.swift
+//  SurfaceIntegrityChecker
+//
+//  Created by Himanshu on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SurfaceIntegrityCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SurfaceIntegrityCheckerTests/SurfaceIntegrityCheckerTests.swift
@@ -0,0 +1,16 @@
+//
+//  SurfaceIntegrityCheckerTests.swift
+//  SurfaceIntegrityCheckerTests
+//
+//  Created by Himanshu on 8/5/25.
+//
+
+import Testing
+
+struct SurfaceIntegrityCheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
