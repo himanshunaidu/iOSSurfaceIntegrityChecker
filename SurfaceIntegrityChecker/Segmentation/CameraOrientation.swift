ff932dfb8a8a83a31a50773dd542a105cae26405
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-08-25 23:44:19 -0700

    Add camera orientation-related logic

---
+++ SurfaceIntegrityChecker/Segmentation/CameraOrientation.swift
@@ -0,0 +1,62 @@
+//
+//  CameraOrientation.swift
+//  SurfaceIntegrityChecker
+//
+//  Created by Himanshu on 8/25/25.
+//
+import UIKit
+import CoreGraphics
+
+class CameraOrientation {
+    
+    static func getCGImageOrientationForBackCamera(currentDeviceOrientation: UIDeviceOrientation) -> CGImagePropertyOrientation {
+        switch currentDeviceOrientation {
+            case .portrait:
+                return .right                // Camera is rotated 90° CW to be upright
+            case .portraitUpsideDown:
+                return .left                 // Camera is rotated 90° CCW
+            case .landscapeLeft:
+                return .up                   // (Home button on the right) Camera is not rotated.
+            case .landscapeRight:
+                return .down                 // (Home button on the left) Camera is rotated 180°.
+            default:
+                return .right               // Fallback to portrait
+        }
+    }
+    
+    static func getCGImageReverseOrientationForBackCamera(currentDeviceOrientation: UIDeviceOrientation) -> CGImagePropertyOrientation {
+        switch currentDeviceOrientation {
+        case .portrait:
+            return .left                 // Camera is rotated 90° CCW to revert to original orientation
+        case .portraitUpsideDown:
+            return .right                // Camera is rotated 90° CW to revert to original orientation
+        case .landscapeLeft:
+            return .up                 // Home button on the right, camera is rotated 180°
+        case .landscapeRight:
+            return .down                   // Home button on the left, camera is rotated 180°
+        default:
+            return .left                // Fallback to portrait
+        }
+    }
+    
+    static func getUIImageOrientationForBackCamera(currentDeviceOrientation: UIDeviceOrientation) -> UIImage.Orientation {
+        switch currentDeviceOrientation {
+            case .portrait:
+                return .right
+            case .portraitUpsideDown:
+                return .left
+            case .landscapeLeft:
+                return .up
+            case .landscapeRight:
+                return .down
+            default:
+                return .right
+        }
+    }
+    
+    // Since people tend to hold devices in portrait mode by default when using the camera,
+    // we can assume that the camera is in portrait mode when the device orientation is unknown.
+    static func isLandscapeOrientation(currentDeviceOrientation: UIDeviceOrientation) -> Bool {
+        return currentDeviceOrientation == .landscapeLeft || currentDeviceOrientation == .landscapeRight
+    }
+}
